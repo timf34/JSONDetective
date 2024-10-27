40be958919a791d912fbfedf175a4552d188df57
timf34 <farrelti@tcd.ie>
2024-10-27 22:46:40 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="jsondetective",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "rich>=13.0.0",
